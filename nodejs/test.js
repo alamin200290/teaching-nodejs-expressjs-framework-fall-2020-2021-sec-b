932a3fc857bf0298f96701ffe2e7aac2791e1a22
sarwar_sikder <amin.se.aiub@gmail.com>
2020-10-21 13:58:49 +0600

    node intor

---
+++ nodejs/test.js
@@ -0,0 +1,26 @@
+
+/*console.log('hello from nodejs...');
+console.log('hello from nodejs...');
+console.log('hello from nodejs...');
+console.log('hello from nodejs...');*/
+
+/*setTimeout(function(){
+	console.log("3s");
+}, 3000);
+
+setTimeout(function(){
+	console.log("5s");
+}, 5000);
+
+setTimeout(function(){
+	console.log("4s");
+}, 4000);
+
+setTimeout(function(){
+	console.log("6s");
+}, 6000);
+
+setTimeout(function(){
+	console.log("2s");
+}, 2000);
+*/
