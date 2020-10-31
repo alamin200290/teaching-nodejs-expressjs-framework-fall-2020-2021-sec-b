c98256663d53838eb88bf3422613185b7b899422
sarwar_sikder <amin.se.aiub@gmail.com>
2020-10-31 17:21:30 +0600

    http core module

--- nodejs/module/core-module/http/app.js
+++ nodejs/module/core-module/http/app.js
@@ -1,7 +1,33 @@
 const http = require('http');
+const fs = require('fs');
 
-var server = http.createServer();
+var server = http.createServer((request, response)=>{
+	//console.log(request.url);
+	//console.log(request.method);
 
+	/*response.write('<h1>Hello from node Http server...</h1>');
+	response.end()*/
+
+	if(request.url == '/'){
+		response.write('Index page');
+		response.end();
+	}else if(request.url == '/home'){
+
+		//response.writeHead(200, {'content-type' : 'text/plain'});
+		//response.write("<h3>Home page</h3>");
+		//response.end();
+
+		/*var data = fs.readFileSync('test.html').toString();
+		response.write(data);
+		response.end();*/
+
+		fs.createReadStream('test.html').pipe(response);
+
+	}else{
+		response.write('<h3>404 invalid request</h3>');
+		response.end()
+	}
+});
 
 server.listen(3000);
 console.log('server started at 3000...');
