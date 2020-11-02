f8562f699bb680e027c6a9c61f89bd9f9591fccd
sarwar_sikder <amin.se.aiub@gmail.com>
2020-11-02 14:06:49 +0600

    view engine+data passing

--- nodejs/express/app.js
+++ nodejs/express/app.js
@@ -1,15 +1,29 @@
-const express = require('express');
-const app = express();
+const express 	= require('express');
+const app 		= express();
 
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
 	res.send('Hello from express server');	
 });
 
 app.get('/home', (req, res)=>{
-	res.send('this is home page');
+
+	var data = {
+		name: 'xyz',
+		id: 16
+	};
+
+	res.render('test', data);
 })
 
+app.get('/login', (req, res)=>{
+	res.render('login/index')
+})
+
+app.post('/login', (req, res)=>{
+	res.send('this is login page');
+})
 
 app.listen(3000, (error)=>{
 	console.log('express server started at 3000...');
