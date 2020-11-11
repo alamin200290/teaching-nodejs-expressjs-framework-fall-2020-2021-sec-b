df5b05f658dc26e6e1f330fde69c8ab0e6dda3be
sarwar_sikder <amin.se.aiub@gmail.com>
2020-11-11 12:30:17 +0600

    database models file

--- nodejs/MVC APP/controller/home.js
+++ nodejs/MVC APP/controller/home.js
@@ -1,12 +1,11 @@
 const express 	= require('express');
-const mysql 	= require('mysql');
+const db 		= require.main.require('./models/db');
 const router 	= express.Router();
 
 router.get('/', (req, res)=>{
 	
 	//req.session.uname != null
 	if(req.cookies['uname'] != null){
-		
 		res.render('home/index');
 	}else{
 		res.redirect('/login');
@@ -16,28 +15,9 @@ router.get('/', (req, res)=>{
 
 router.get('/userlist', (req, res)=>{
 
-	var connection = mysql.createConnection({
-	  host     : '127.0.0.1',
-	  user     : 'root',
-	  password : '',
-	  database : 'node1'
-	});
-	 
-	connection.connect(function(err) {
-	  if (err) {
-	    console.error('error connecting: ' + err.stack);
-	    return;
-	  }	 
-	  console.log('connected as id ' + connection.threadId);
-	});
-
 	var sql = "select * from user ";
-	connection.query(sql, function (error, results) { 
+	db.getResults(sql, function(results){
 		res.render('home/userlist', {userlist: results});
-	});
-
-	connection.end(function(err) {
-		console.log('connection closed!');	  
 	});
 });
 
